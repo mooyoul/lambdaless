c7c68db4236f3ca5e196f986599f52a39b6d6fe3
MooYeol Prescott Lee <mooyoul@gmail.com>
2019-11-05 05:32:04 +0900

    add lambdaless email subscription service

---
+++ email-subscription/README.md
@@ -0,0 +1,162 @@
+# Lambdaless Proxy
+
+A HTTP Proxy implemented without Lambda usage
+
+### Why?
+
+You may need to proxy requests, due to following reasons:
+
+- Blocked Access (e.g. Blocked Source IP, Blocked Source CIDR ...)
+- Accelerate server access (e.g. Make open graph crawler to faster)
+- Secure insecure resources (e.g. Prevent [Mixed Content](https://developers.google.com/web/fundamentals/security/prevent-mixed-content/what-is-mixed-content?hl=en) issue) 
+ 
+ 
+![Example](./assets/blocked.png) 
+
+At [Vingle](https://www.vingle.net), We have to proxy certain open graph scrap requests to bypass geo restrictions of some websites.
+
+Previously, We had simple lambda function that proxies requests. 
+
+For example: 
+
+```typescript
+import axios from "axios";
+
+export async function handler(event: Event) {
+  const { url } = event.body;
+ 
+  const response = await axios({
+    method: "GET",
+    url,
+    headers: {
+      "User-Agent": "facebookexternalhit/1.1"
+    },
+    timeout: 15,
+  });
+
+  return { 
+    statusCode: response.status,
+    headers: response.headers,
+    body: res.data,
+  };
+}
+```
+
+It worked pretty good, but Lambda based proxy has some downsides:
+
+- We have to monitor Lambda invocations, latencies, failures ...
+- We have to pay for Lambda Cost - Most time of lambda invocation just waits for response from upstream
+- We have to maintenance Lambda function and dependencies - Node.js Lambda runtime reaches EOL, New version of axios is released ...   
+
+So we decided to remove Lambda usage in this use-case, and We could make this better!
+
+### How?
+
+Just use HTTP_PROXY integration with pass-through option and abstract proxy endpoints by using AWS CDK. 
+
+
+### Getting Started
+
+Clone lambdaless repository:
+
+```bash
+$ git clone https://github.com/mooyoul/lambdaless.git
+```
+
+Navigate to `proxy` directory:
+
+```bash
+$ cd lambdaless/proxy
+```
+
+Install required dependencies:
+
+```bash
+$ npm ci
+``` 
+
+Edit proxy configurations:
+
+```bash
+$ vi src/index.ts
+```
+
+Deploy
+
+```bash
+$ npm run cdk -- deploy
+$ # OR
+$ env AWS_PROFILE=myprofile AWS_REGION=us-east-1 npm run cdk -- deploy
+```
+
+Done! 🎉
+
+### Example
+
+```typescript
+const proxy = new Proxy(this, "Proxy", {
+  apiName: "lambdaless-proxy",
+  endpointType: EndpointType.EDGE,
+});
+
+proxy.addProxy("ipify", "https://api.ipify.org", "GET");
+```
+
+This will create one resource that proxies `/ipify` path to `https://api.ipify.org
+
+Example matches:
+
+| Endpoint | Mapped to |
+| -------- | --------- |
+| https://API_ID.execute-api.REGION.amazonaws.com/prod/ipify | https://api.ipify.org/ |
+| https://API_ID.execute-api.REGION.amazonaws.com/prod/ipify/foo/bar?baz | https://api.ipify.org/foo/bar?baz |
+
+
+### Testing
+
+Since there are no any business logic, Testing is not necessary.
+
+
+### Debugging
+
+![tester](./assets/tester.png)
+
+Use API Gateway built-in API Tester. You can inspect full request/response and execution logs.
+
+See: https://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-test-method.html
+
+
+### Troubleshoot
+
+##### Deploy failure with CloudWatch Logs Role message
+
+![error](./assets/failure.png)
+
+> CloudWatch Logs role ARN must be set in account settings to enable logging.
+
+In this case, You should configure IAM Role for logging.
+
+Follow below two steps in [this document](https://aws.amazon.com/premiumsupport/knowledge-center/api-gateway-cloudwatch-logs/):
+
+- "Create an IAM role for logging to CloudWatch" section
+- "Add the IAM role in the API Gateway console"
+
+> NOTE: "Enable logging for your API and stage" section is not required.
+
+
+### Challenges / Wishlists
+
+##### "Dynamic" forward proxy
+
+It would be nice if we can implement forward proxy
+
+For example, 
+
+- https://API_ID.execute-api.REGION.amazonaws.com/prod/https://www.example.com/foo/bar/baz
+
+will proxy requests to
+
+- https://www.example.com/foo/bar/baz
+
+I haven't tried this so let me know it can be implemented in Lambdaless, 
+or you can try my alternative project: [proxyfront](https://github.com/mooyoul/proxyfront) - Another approach to implement forward proxy using Lambda@Edge.
---
+++ email-subscription/src/subscription.ts
@@ -0,0 +1,144 @@
+import * as cdk from "@aws-cdk/core";
+
+import * as apigw from "@aws-cdk/aws-apigateway";
+import * as ddb from "@aws-cdk/aws-dynamodb";
+import * as iam from "@aws-cdk/aws-iam";
+
+import { stripIndent } from "common-tags";
+
+export interface SubscriptionServiceProps {
+  readonly apiName: string;
+  readonly endpointType: apigw.EndpointType;
+  readonly tableName: string;
+}
+
+export class SubscriptionService extends cdk.Construct {
+  public readonly api: apigw.RestApi;
+  public readonly table: ddb.Table;
+  public readonly exeutionRole: iam.Role;
+
+  constructor(scope: cdk.Construct, id: string, props: SubscriptionServiceProps) {
+    super(scope, id);
+
+    // API Gateway for Subscription API
+    this.api = new apigw.RestApi(this, "API", {
+      restApiName: props.apiName,
+      endpointTypes: [props.endpointType],
+      deployOptions: {
+        loggingLevel: apigw.MethodLoggingLevel.INFO,
+      },
+    });
+
+    this.table = new ddb.Table(this, "Table", {
+      tableName: props.tableName,
+      partitionKey: {
+        name: "email",
+        type: ddb.AttributeType.STRING,
+      },
+      billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    this.exeutionRole = new iam.Role(this, "APIExecutionRole", {
+      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com"),
+      inlinePolicies: {
+        "allow-ddb-access": new iam.PolicyDocument({
+          statements: [new iam.PolicyStatement({
+            actions: ["dynamodb:PutItem"],
+            effect: iam.Effect.ALLOW,
+            resources: [this.table.tableArn],
+          })],
+        }),
+      }
+    });
+
+    const requestModel = this.api.addModel("SubscriptionRequestModel", {
+      contentType: "application/json",
+      modelName: "SubscriptionRequest",
+      schema: {
+        title: "subscribeRequest",
+        type: apigw.JsonSchemaType.OBJECT,
+        required: ["email"],
+        properties: {
+          email: {
+            type: apigw.JsonSchemaType.STRING,
+            format: "email",
+            minLength: 1,
+            maxLength: 256,
+          },
+        },
+      },
+    });
+
+    const validator = new apigw.RequestValidator(this, "RequestValidator", {
+      restApi: this.api,
+      validateRequestBody: true,
+    });
+
+    const subscriptionResource = this.api.root.addResource("subscriptions");
+
+    subscriptionResource.addMethod("POST", new apigw.AwsIntegration({
+      service: "dynamodb",
+      integrationHttpMethod: "POST",
+      action: "PutItem",
+      options: {
+        credentialsRole: this.exeutionRole,
+        passthroughBehavior: apigw.PassthroughBehavior.NEVER,
+        requestTemplates: {
+          "application/json": stripIndent`
+            {
+                "TableName": "${this.table.tableName}",
+                "Item": {
+                    "email": {
+                        "S": "$input.path('$.email')"
+                    },
+                    "createdAt": {
+                        "N": "$context.requestTimeEpoch"
+                    }
+                },
+                "ConditionExpression": "attribute_not_exists(email)"
+            }
+          `,
+        },
+        integrationResponses: [{
+          // Pass-through successful responses
+          statusCode: "200",
+          responseParameters: {
+            "method.response.header.Content-Type": "'application/json'",
+          },
+          responseTemplates: {
+            "application/json": "$input.body",
+            "application/x-amz-json-1.0": "$input.body",
+          },
+        }, {
+          // Pass-through error responses
+          selectionPattern: "[45][0-9][0-9]",
+          statusCode: "422",
+          responseParameters: {
+            "method.response.header.Content-Type": "'application/json'",
+          },
+          responseTemplates: {
+            // Pass-through
+            "application/json": "$input.body",
+            "application/x-amz-json-1.0": "$input.body",
+          },
+        }],
+      },
+    }), {
+      requestModels: {
+        "application/json": requestModel,
+      },
+      requestValidator: validator,
+      methodResponses: [{
+        statusCode: "200",
+        responseParameters: {
+          "method.response.header.Content-Type": true,
+        },
+      }, {
+        statusCode: "422",
+        responseParameters: {
+          "method.response.header.Content-Type": true,
+        },
+      }]
+    });
+  }
+}
